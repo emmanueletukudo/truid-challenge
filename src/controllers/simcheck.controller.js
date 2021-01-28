602dc9c6243addcdbd7da3ab2cd5d7a3156fb18c
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-01-28 02:22:34 +0100

    created simcheck controller

--- src/controllers/simcheck.controller.js
+++ src/controllers/simcheck.controller.js
@@ -5,15 +5,76 @@
  * Ensure you run npm install if you have not done that already.
  */
 
-
-const simCheckController = {
-  check: async (req, res, next) => {
-    try {
-      
-    } catch (error) {
-      console.log(error);
+const check =  async () => {
+  try {
+    let { phone } = req.body;
+    if (!phone) {
+      return res
+        .status(422)
+        .json({ phone: "The phone number field is required" });
     }
-  },
+    const checkRes = await simCheck(phone);
+
+    // res.json({
+    //     no_sim_change: simCheck.no_sim_change,
+    //     last_sim_change_at: simCheck.last_sim_change_at
+    // })
+    res.json(checkRes);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-module.exports = simCheckController;
+const simCheck = async () => {
+  try {
+    const reqObj = {
+      URI: `${base_URL}/sim_check/v0.1/checks`,
+      params: { phone_number: phone },
+    };
+    let token = (await getAccessToken()).access_token;
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    };
+
+    const simCheckRes = await axios.post(reqObj.URI, reqObj.params, {
+      headers: headers,
+    });
+
+    return simCheckRes.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const getAccessToken = async () => {
+  try {
+    const reqObj = {
+      URI: `${base_URL}/oauth2/v1/token`,
+      //convert params to string suing qs package as recommended by the doc.
+      params: qs.stringify({
+        grant_type: "client_credentials",
+        scope: "sim_check",
+      }),
+    };
+    //convert credentials to base64
+    // console.log(trueCredentials.credentials[0].client_id);
+    let clientDetails = `${trueCredentials.credentials[0].client_id}:${trueCredentials.credentials[0].client_secret}`;
+    let encoded_credentials = Buffer.from(clientDetails).toString("base64");
+
+    const headers = {
+      Authorization: `Basic ${encoded_credentials}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+
+    const tokenRes = await axios.post(reqObj.URI, reqObj.params, {
+      headers: headers,
+    });
+    //console.log(tokenRes.data);
+    return tokenRes.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {check, simCheck, getAccessToken};
